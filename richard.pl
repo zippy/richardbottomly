68c3c964d87828d46715f486d88b0790a18f8a1e
Sam Cooley <sam.cooley@cooleyassociates.com>
2018-04-21 04:30:41 +0000

    fixes to ansible plus looping

--- richard.pl
+++ richard.pl
@@ -2,50 +2,70 @@
 # Copyleft 2018 (totally free)
 use JSON::Parse 'parse_json';
 
-my $fname = "last.txt";
-local $/=undef;
-my $start_block = 0;
-my $eth = 0;
-if (open FILE, $fname) {
-    my $dataj = <FILE>;
-    close FILE;
-    my $data = &parse_json($dataj);
-    $eth = $data->{"eth"};
-    $start_block = $data->{"startBlock"};
+while(true) {
+    &check();
+    sleep(60);
 }
 
-my $addr = "0xa28FC2102Da86e424B37Ad86C7Bc7855fC441239";
-my $url = "http://api.etherscan.io/api?module=account&action=txlist&address=$addr&startblock=$start_block&endblock=99999999&sort=asc&apikey=31DCRU6XIZCB6BUR65BCPYK77MEPJHM4HW";
-my $raw = `curl -X GET "$url"`;
-
-my $j = &parse_json($raw);
-my $transactions = $j->{"result"};
-my $msg = "";
-foreach $tx (@$transactions) {
-    if (!$tx->{'isError'}) {
-        my $v = $tx->{'value'};
-        $v /= 1000000000000000000;
-        $msg .= "$v ETH ";
-        $msg .= "from: $tx->{'from'}\n";
-        $eth += $v
+sub check {
+    my $fname = "last.txt";
+    local $/=undef;
+    my $start_block = 0;
+    my $eth = 0;
+    if (open FILE, $fname) {
+	my $dataj = <FILE>;
+	close FILE;
+	my $data = &parse_json($dataj);
+	$eth = $data->{"eth"};
+	$start_block = $data->{"startBlock"};
     }
-    $start_block = $tx->{'blockNumber'};
+
+    my $addr = "0xa28FC2102Da86e424B37Ad86C7Bc7855fC441239";
+    my $url = "http://api.etherscan.io/api?module=account&action=txlist&address=$addr&startblock=$start_block&endblock=99999999&sort=asc&apikey=31DCRU6XIZCB6BUR65BCPYK77MEPJHM4HW";
+    my $raw = `curl -X GET "$url"`;
+
+    my $j = &parse_json($raw);
+    my $transactions = $j->{"result"};
+    my $msg = "";
+    foreach $tx (@$transactions) {
+	if (!$tx->{'isError'}) {
+	    my $v = $tx->{'value'};
+	    $v /= 1000000000000000000;
+	    $eth += $v;
+	    $msg .= << "EOM";
+| **Amount** | **$v ETH** |
+|------------|------------|
+| **From**   | **$tx->{'from'}** |
+| Running Total | $eth ETH |
+EOM
+
+	}
+	$start_block = $tx->{'blockNumber'};
+    }
+    open(my $fh, '>', $fname);
+    print $fh "{\"eth\":$eth,\"startBlock\":".($start_block+1)."}";
+    close $fh;
+
+    if ($msg ne "") {
+	my $post = <<"END_MSG";
+Ooh. Someone seems to have sent ETH to your ICO! 
+
+$msg
+END_MSG
+	&post($post);
+    }
 }
-print $msg;
-print "TOTAL: $eth ETH\n";
-open(my $fh, '>', $fname);
-print $fh "{\"eth\":$eth,\"startBlock\":".($start_block+1)."}";
-close $fh;
 
-&post("test");
-
-my $PLAYBOOKS_HOME = "/root/playbooks";
-my $BOT_VARS = "richard.bottomly";
 sub post {
+    my $PLAYBOOKS_HOME = "/root/playbooks";
+    my $BOT_VARS = "richard.bottomly";
+
     my $msg = shift;
     my $cmd = <<"END_CMD";
-ansible-playbook -i ",localhost" -c local $PLAYBOOKS_HOME/mattermost-ansible/mattermost-post.yml \
-        --extra-vars="@/root/playbooks/mattermost-ansible/mattermost-post_$BOT_VARS.vars" \
-        --extra-vars="mattermost_post_text='$msg'"
+ansible-playbook -i ",localhost" -c local $PLAYBOOKS_HOME/mattermost-ansible/mattermost-post.yml --extra-vars="@/root/playbooks/mattermost-ansible/mattermost-post_$BOT_VARS.vars" --extra-vars="mattermost_post_text='$msg'"
 END_CMD
+   # print $cmd;
+   # print "\n";
+    # print `$cmd`;
+    $cmd;
 }
