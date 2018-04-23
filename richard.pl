b11800a5cb71c920846a4003f7fb164490ab9afd
Sam Cooley <sam.cooley@cooleyassociates.com>
2018-04-23 15:30:06 +0000

    fixes

--- richard.pl
+++ richard.pl
@@ -1,6 +1,7 @@
 # a bot to send ETH sale notifications to mattermost via a webhook
 # Copyleft 2018 (totally free)
 use JSON::Parse 'parse_json';
+use JSON::Parse 'valid_json';
 
 while(true) {
     &check();
@@ -23,7 +24,10 @@ sub check {
     my $addr = "0xa28FC2102Da86e424B37Ad86C7Bc7855fC441239";
     my $url = "http://api.etherscan.io/api?module=account&action=txlist&address=$addr&startblock=$start_block&endblock=99999999&sort=asc&apikey=31DCRU6XIZCB6BUR65BCPYK77MEPJHM4HW";
     my $raw = `curl -X GET "$url"`;
-
+    if (!&valid_json($raw)) {
+	print "invalid json: $raw";
+	return;
+    }
     my $j = &parse_json($raw);
     my $transactions = $j->{"result"};
     my $msg = "";
@@ -37,6 +41,7 @@ sub check {
 |------------|------------|
 | **From**   | **$tx->{'from'}** |
 | Running Total | $eth ETH |
+
 EOM
 
 	}
@@ -47,10 +52,15 @@ EOM
     close $fh;
 
     if ($msg ne "") {
-	my $post = <<"END_MSG";
-Ooh. Someone seems to have sent ETH to your ICO! 
+	my $fortune = `/usr/games/fortune -s`;
+	$fortune =~ s/\n/ /g;
 
+	#Ooh. Someone seems to have sent ETH to your ICO! 
+	my $post = <<"END_MSG";
 $msg
+
+$fortune
+
 END_MSG
 	&post($post);
     }
@@ -60,12 +70,15 @@ sub post {
     my $PLAYBOOKS_HOME = "/root/playbooks";
     my $BOT_VARS = "richard.bottomly";
 
+    
     my $msg = shift;
+    $msg =~ s/"/\\"/g;
+    $msg =~ s/'/\\'/g;
     my $cmd = <<"END_CMD";
 ansible-playbook -i ",localhost" -c local $PLAYBOOKS_HOME/mattermost-ansible/mattermost-post.yml --extra-vars="@/root/playbooks/mattermost-ansible/mattermost-post_$BOT_VARS.vars" --extra-vars="mattermost_post_text='$msg'"
 END_CMD
    # print $cmd;
    # print "\n";
     # print `$cmd`;
-    $cmd;
+    `$cmd`;
 }
